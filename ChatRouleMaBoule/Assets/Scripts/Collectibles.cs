baa18c61048294e8581ef224e08b147fedcce8ad
DrPickle3 <ronc1402@usherbrooke.ca>
2025-11-15 18:26:20 -0500

    level Manager yeyeaehaeaehaehhhooooo

--- ChatRouleMaBoule/Assets/Scripts/Collectibles.cs
+++ ChatRouleMaBoule/Assets/Scripts/Collectibles.cs
@@ -1,11 +1,30 @@
 ﻿using UnityEngine;
-using System;
-using Unity.Mathematics;
 
 namespace DefaultNamespace
 {
     public class Collectibles : MonoBehaviour
     {
+        private LevelManager _levelManager;
+
+        public void Start()
+        {
+            _levelManager = FindFirstObjectByType<LevelManager>();
+        }
         
+        public void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Boule"))
+            {
+                if (gameObject.CompareTag("Star"))
+                {
+                    _levelManager.StarCollected();
+                }
+                else
+                {
+                    _levelManager.CoinCollected();
+                }
+                Destroy(gameObject);
+            }
+        }
     }
 }
--- ChatRouleMaBoule/Assets/Scripts/LevelManager.cs
+++ ChatRouleMaBoule/Assets/Scripts/LevelManager.cs
@@ -1,31 +1,53 @@
 using System.IO;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using Unity.VisualScripting;
 
 namespace DefaultNamespace
 {
-    public class LevelsJSON
-    {
-        [Serialize]
-        public bool[] levels {get; set;}
-    }
-
     public class LevelManager : MonoBehaviour
     {
-
+        private bool[] Levels { get; set; }
         public void Start()
         {
+            var textLevels = Resources.Load<TextAsset>("levels").text;
+
+            Levels = textLevels
+                .Split(',')
+                .Select(v => v.Trim().ToLower() == "true")
+                .ToArray();
+        }
+
+        public void CoinCollected()
+        {
+            // TODO : Update Score TMP
+        }
+        
+        public void StarCollected()
+        {
+            // TODO : Update Score TMP
+            CompleteLevel();
         }
 
         private void CompleteLevel()
         {
+            var index = SceneManager.GetActiveScene().buildIndex;
+            Levels[index - 1] = true;
             
+            SaveProgress();
+            
+            SceneManager.LoadScene(index + 1);
+        }
+
+        private void SaveProgress()
+        {
+            var text = string.Join(",", Levels.Select(b => b ? "true" : "false"));
+            File.WriteAllText(Application.dataPath + "/Resources/levels.txt", text);
         }
 
         private void ResetProgress()
         {
-            
+            File.WriteAllText(Application.dataPath + "/Resources/levels.txt", "false,false,false");
         }
     }
 }
