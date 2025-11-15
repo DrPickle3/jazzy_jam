c3c0d72fc3443d75b647d86b32a4b73c5d81d74a
Lea <lea.roy@outlook.com>
2025-11-15 15:01:38 -0500

    laisse moi commit

--- ChatRouleMaBoule/Assets/Scripts/LevelManager.cs
+++ ChatRouleMaBoule/Assets/Scripts/LevelManager.cs
@@ -1,9 +1,43 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using Newtonsoft.Json;
+using Unity.VisualScripting;
 
 namespace DefaultNamespace
 {
+    public class LevelsJSON
+    {
+        [Serialize]
+        public bool[] levels {get; set;}
+    }
+
     public class LevelManager : MonoBehaviour
     {
-        
+        private LevelsJSON _levels;
+
+        public void Start()
+        {
+            string jsonLevels = File.ReadAllText("levels.json");
+            _levels = JsonConvert.DeserializeObject<LevelsJSON>(jsonLevels);
+        }
+
+        static void test()
+        {
+            
+        }
+
+        private void CompleteLevel()
+        {
+            
+        }
+
+        private void ResetProgress()
+        {
+            var lvls = new LevelsJSON();
+            string jsonString = JsonSerializer.Serialize(lvls);
+            File.WriteAllText("levels.json", jsonString);
+        }
     }
 }
