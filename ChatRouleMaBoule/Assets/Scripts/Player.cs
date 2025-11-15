7b7e75d36cac783719f18aa47e984a274fe93501
Lea <lea.roy@outlook.com>
2025-11-15 12:43:46 -0500

    player

--- ChatRouleMaBoule/Assets/Scripts/Player.cs
+++ ChatRouleMaBoule/Assets/Scripts/Player.cs
@@ -1,9 +1,16 @@
+using System;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     private InputActions _inputActions;
     private GameObject _boule;
+    private GameObject _camera;
+    private Animator _catAnimator;
+    
+    private float SPEED = 1f;
+    private float RUNNING_SPEED = 5f;
     
     public void Awake()
     {
@@ -14,12 +21,28 @@ public class Player : MonoBehaviour
     {
         _inputActions.Enable();
         _boule = GameObject.FindWithTag("Boule");
+        _catAnimator = gameObject.GetComponentInChildren<Animator>();
+
     }
     public void Update()
     {
         Vector2 curMoveInput = _inputActions.Player.Move.ReadValue<Vector2>();
+        bool isRunning = _inputActions.Player.Sprint.IsPressed();
         curMoveInput.Normalize();
 
-        _boule.transform.position += new Vector3(curMoveInput.x, 0, curMoveInput.y);
+        var speed = isRunning ? RUNNING_SPEED : SPEED;
+        var move = curMoveInput * speed * Time.deltaTime;
+        var forward = new Vector3(move.x, 0, move.y);
+        gameObject.transform.position += forward;
+        _catAnimator.SetBool("isRunning", isRunning);
+        _catAnimator.SetBool("isMoving", false);
+
+        if (math.lengthsq(forward) > float.Epsilon)
+        {
+            if(!isRunning)
+                _catAnimator.SetBool("isMoving", true);
+            
+            _boule.transform.rotation = Quaternion.LookRotation(forward, math.up());
+        }
     }
 }
