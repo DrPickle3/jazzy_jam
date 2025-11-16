2745b5659f964862858a4fa57ddc32ca2300b6d3
DrPickle3 <ronc1402@usherbrooke.ca>
2025-11-15 19:20:23 -0500

    UI

--- ChatRouleMaBoule/Assets/mainmenu.cs
+++ ChatRouleMaBoule/Assets/mainmenu.cs
@@ -5,7 +5,7 @@ public class mainmenu : MonoBehaviour
 {
    public void PlayGame()
    {
-    SceneManager.LoadScene("LevelSelect");
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
    }
    public void QuitGame()
    {
